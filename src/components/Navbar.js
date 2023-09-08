11d1f17f976f20d907521c42adc34904c304e187
shilpaVero03 <veronicashilu03@gmail.com>
2023-09-08 18:52:25 +0530

    navbar changes done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,9 @@ const Navbar = () => {
       <li className="nav-item">
         <a className="nav-link" href="#">About Me</a>
       </li>
-  
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Experience</a>
       </li>
